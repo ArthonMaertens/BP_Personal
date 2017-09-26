3274d4f5b24eae2f3906830d41d73f67ddea1a8d
arthon maertens <arthon.maertens@student.howest.be>
2017-09-26 12:02:46 +0200

    oefeningen

--- Oef_04.py
+++ Oef_04.py
@@ -1,7 +1,17 @@
 #Oef 04 som van n
 
 getal = input("Geef een getal op: ")
-getal = int (getal)
-getal1 = getal*getal
-getal2 = getal*getal*getal
-print(getal , getal1, getal2)
+
+
+getal = getal
+getal1 = getal+getal
+getal2 = getal+getal+getal
+
+getal = int(getal)
+getal1 = int(getal1)
+getal2 = int(getal2)
+
+totaal = getal + getal1 + getal2
+
+
+print(totaal)
---
+++ Oef_05.py
@@ -0,0 +1,9 @@
+#Oef 05 (x+y) * (x+y)
+
+getal1 = input("Geef een getal op: ")
+getal2 = input("Geef een getal op: ")
+
+getal1 = int(getal1)
+getal2 = int(getal2)
+
+print((getal1 + getal2) * (getal1 + getal2))
